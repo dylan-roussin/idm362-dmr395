73f1a339e8134be0fa5b8b22087e25aee7886a2c
Dylan Roussin <dylanroussin@gmail.com>
2023-02-09 11:58:23 -0500

    classwork

--- idm362-dmr395/ViewController.swift
+++ idm362-dmr395/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    
+    var vCounter:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        displayLabel.textColor = UIColor(named: "Prince")
     }
 
-
+    @IBAction func addBtn(_ sender: Any) {
+        vCounter += 1
+        displayLabel.text = String(vCounter)
+    }
+    
+    
+    @IBAction func nameBtn(_ sender: Any) {
+        displayLabel.text = "Well Hello There \(nameText.text!)"
+    }
+    // Disable keyboard when user touches elsewhere
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
 }
 
